ce9b39cb129998b8bf7b4b3e546c1a9a7768fc77
Thinkenterprise <michael.schaefer@msg-systems.com>
2019-03-07 11:12:25 +0100

    Security Bug Fix and added context feature

--- curl.sh
+++ curl.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -X POST 'http://localhost:4000/graphql' -H 'Content-Type: application/json' -d '{"query":"{routes{id}}"}'
+curl -u user:password -X POST 'http://localhost:4000/graphql' -H 'Content-Type: application/json' -d '{"query":"{routes{id}}"}'
---
+++ src/main/java/com/thinkenterprise/security/GraphQLBasicSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.thinkenterprise.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@Profile("httpbasic")
+public class GraphQLBasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+      @Override
+      protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and().httpBasic();
+    
+      }
+    
+      @Override
+      protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder().encode("password"))
+            .roles("ADMIN");
+      }
+    
+      @Bean
+      PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+      }
+}
