67bc450644934e22832d1881b4bb47e97cfd8fbb
Mark Hoja <mark.hoja@msg.group>
2019-04-05 11:00:34 +0200

    fix

--- src/test/java/com/thinkenterprise/test/GraphQLRouteQueryTest.java
+++ src/test/java/com/thinkenterprise/test/GraphQLRouteQueryTest.java
@@ -14,10 +14,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode=ClassMode.BEFORE_EACH_TEST_METHOD)
 public class GraphQLRouteQueryTest {
 
     @Autowired
